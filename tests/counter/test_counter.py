9124e0607bf534edf230a0333f76c3a9a946b5fa
jvdionizio <dionizio145@gmail.com>
2023-01-03 10:16:02 -0300

    feat: test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+path = "data/jobs.csv"
 
 
 def test_counter():
-    pass
+
+    assert count_ocurrences(path, "Python") == 1639
+    assert count_ocurrences(path, "Javascript") == 122
