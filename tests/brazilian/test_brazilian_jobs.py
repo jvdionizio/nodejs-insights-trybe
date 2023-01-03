1138c3ea7c9a1a240412332dc6057e723c98caad
jvdionizio <dionizio145@gmail.com>
2023-01-03 12:05:13 -0300

    feat: test brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    translated_list = read_brazilian_file(path)
+    for report in translated_list:
+        assert "title" in report
+        assert "salary" in report
+        assert "type" in report
+        assert "titulo" not in report
+        assert "salario" not in report
+        assert "tipo" not in report
